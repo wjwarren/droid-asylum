fac06486cd70637de540281d3ed3966bb1dc21b8
Wijnand Warren <god@ansuz.nl>
2013-06-22 18:59:05 +0200

    Removing unused files.

--- src/com/musselwhizzle/mvc/events/Event.java
+++
@@ -1,8 +0,0 @@
-package com.musselwhizzle.mvc.events;
-
-public interface Event {
-	
-	public String getType();
-	public Object getSource();
-	public void setSource(Object source);
-}
--- src/com/musselwhizzle/mvc/events/EventListener.java
+++
@@ -1,9 +0,0 @@
-package com.musselwhizzle.mvc.events;
-
-
-
-public interface EventListener {
-	
-	void onEvent(Event event);
-
-}
